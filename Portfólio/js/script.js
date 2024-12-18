a9f5a203a2b69f2fe1ed6d1f3edbff11d807f4f8
Luiz <luizdelimaribas@live.com>
2024-12-18 19:45:15 -0300

    portifólio para testes

---
+++ Portfólio/js/script.js
@@ -0,0 +1,19 @@
+// transforma o menu hanburguer em x
+const menuhamburguer=document.querySelector('.menu-hamburguer')
+menuhamburguer.addEventListener('click',()=>{
+  toggleMenu();
+});
+
+    function toggleMenu(){
+      const nav=document.querySelector('.nav-responsive');
+      menuhamburguer.classList.toggle('change');
+
+      if(menuhamburguer.classList.contains('change')){
+        nav.style.display = 'block';
+      }
+      else{
+        nav.style.display = 'none';
+      }
+    }
+
+    
